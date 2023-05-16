9ecab4e3068a2ccabf226adab561f31a26c9562f
eyk261 <eden.y.kifle@gmail.com>
2023-05-16 09:55:07 -0400

    add all

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const input = 'Eden Yemane Kifle';
 const vowels = ['a','e','i','o','u'];
 const resultArray = [];
-for(let i=0; i<input.length;i++){
-    console.log(input[i]);
+for(let inputIndex=0; inputIndex<input.length;inputIndex++){
+   // console.log(input[inputIndex]);
+   for(let vowelIndex=0; vowelIndex<vowels.length; vowelIndex++){
+    //console.log(vowels[vowel]);
+    if(input[inputIndex]==='e'){
+        resultArray.push(input[inputIndex]);
+    }
+    if(input[inputIndex]===vowels[vowelsIndex]){
+        //console.log(input[inputIndex]);
+        resultArray.push(input[inputIndex]);
+    }
+   }
 }
+//console.log(resultArray);
